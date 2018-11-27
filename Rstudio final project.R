e1d16051eaabe5dcd43faa65436259543668cb67
braggjn <44878990+braggjn@users.noreply.github.com>
2018-11-27 16:53:39 -0500

    Add files via upload

--- Rstudio final project.R
+++ Rstudio final project.R
@@ -1,3 +1,4 @@
+## Overall Tour ##
 install.packages("gmapsdistance")
 library(gmapsdistance)
 set.api.key("AIzaSyBMO8Hyj6lsza9JLkN0TzCP9cimVxd67n4")
@@ -20,3 +21,40 @@ tour <- solve_TSP(df, 'nn')
 tour_length(tour)
 tour <- as.data.frame(tour)
 
+
+## Tour of West ##
+resultsWest = gmapsdistance(origin = c("Yosemite+National+Park", "Grand+Canyon", "Redwood+National+Park", "Gardiner+MT", "Bryce+Canyon", "Glacier+National+Park", "Badlands+National+Park", "Arches+National+Park", "Zion+National+Park", "Monument+valley+national+park"),
+                        destination = c("Yosemite+National+Park", "Grand+Canyon", "Redwood+National+Park", "Gardiner+MT", "Bryce+Canyon", "Glacier+National+Park", "Badlands+National+Park", "Arches+National+Park", "Zion+National+Park", "Monument+valley+national+park"), 
+                        mode = "driving")
+round(resultsWest$Distance[,2:11] * 0.000621371192, 2)
+West <- round(resultsWest$Distance[,2:11] * 0.000621371192, 2)
+row.names(West) <- colnames(West)
+
+write.csv(West, file="Westdistance.csv")
+
+library(TSP)
+
+West <- as.matrix(West)
+West <- as.ATSP(West)
+tourWest <- solve_TSP(West, 'nn')
+tour_length(tourWest)
+tourWest <- as.data.frame(tourWest)
+
+
+## Tour of East ##
+resultsEast = gmapsdistance(origin = c("Acadia+national+park", "Congaree+National+Park", "Voyageurs+national+park", "Cuyahoga+Valley+national+park", "Great+Smoky+Mountains+National+Park", "Shenandoah+national+park", "Mammoth+Cave+national+park", "Cape+Cod+national+park", "Isle+Royale+national+park", "Dry+Tortugas+national+park"),
+                        destination = c("Acadia+national+park", "Congaree+National+Park", "Voyageurs+national+park", "Cuyahoga+Valley+national+park", "Great+Smoky+Mountains+National+Park", "Shenandoah+national+park", "Mammoth+Cave+national+park", "Cape+Cod+national+park", "Isle+Royale+national+park", "Dry+Tortugas+national+park"), 
+                        mode = "driving")
+round(resultsEast$Distance[,2:11] * 0.000621371192, 2)
+East <- round(resultsEast$Distance[,2:11] * 0.000621371192, 2)
+row.names(East) <- colnames(East)
+
+write.csv(East, file="Eastdistance.csv")
+
+library(TSP)
+
+East <- as.matrix(East)
+East <- as.ATSP(East)
+tourEast <- solve_TSP(East, 'nn')
+tour_length(tourEast)
+tourEast <- as.data.frame(tourEast)
